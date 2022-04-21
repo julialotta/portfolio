d298eb0207fb503098b2e2c402bf2d669692dbba
julialotta <julialottatinglof@gmail.com>
2022-04-21 23:15:33 +0200

    started on main

--- src/partials/main.tsx
+++ src/partials/main.tsx
@@ -1,8 +1,31 @@
+import project from "../assets/img/JL01.png";
+
 function Main() {
   return (
     <main>
       <p>Lorem ipsum</p>
       <div className="pattern"></div>
+      <div className="green"></div>
+      <div className="categoryContainer">
+        <div className="category">
+          <h4>Skolprojekt</h4>
+          <img className="hero" src={project} alt="Tingloef" />
+          <img className="hero" src={project} alt="Tingloef" />
+          <img className="hero" src={project} alt="Tingloef" />
+        </div>
+        <div className="category">
+          <h4>Jobb</h4>
+          <img className="hero" src={project} alt="Tingloef" />
+          <img className="hero" src={project} alt="Tingloef" />
+          <img className="hero" src={project} alt="Tingloef" />
+        </div>
+        <div className="category">
+          <h4>Egna projekt</h4>
+          <img className="hero" src={project} alt="Tingloef" />
+          <img className="hero" src={project} alt="Tingloef" />
+          <img className="hero" src={project} alt="Tingloef" />
+        </div>
+      </div>
     </main>
   );
 }
