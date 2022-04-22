5ebdc996e5618352a68bb87d36887855ac712f76
julialotta <julialottatinglof@gmail.com>
2022-04-22 12:02:26 +0200

    mobile version almost done

--- src/partials/main.tsx
+++ src/partials/main.tsx
@@ -1,12 +1,28 @@
 import project from "../assets/img/JL01.png";
+import cvtree from "../assets/cvtree.png";
 
 function Main() {
   return (
     <main>
-      <p>Lorem ipsum</p>
       <div className="pattern"></div>
       <div className="green"></div>
-      <div className="categoryContainer">
+      <div className="categoryContainerOm">
+        <h3>Om</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+          veritatis esse error cum, pariatur eos quam, molestias commodi nostrum
+          consequatur officiis beatae, doloremque quod dolorem at provident sit
+          dolore aliquam?
+        </p>
+        <div className="images">
+          <img src={cvtree} alt="Cv tree" />
+          <img src={project} alt="Tingloef" />
+        </div>
+      </div>
+      <div className="pattern"></div>
+      <div className="red"></div>
+      <div className="categoryContainerProjekt">
+        <h3>Projekt</h3>
         <div className="category">
           <h4>Skolprojekt</h4>
           <img className="hero" src={project} alt="Tingloef" />
